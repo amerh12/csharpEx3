417a76b10d17d361fa434096397cfe621304b40c
amerh12 <91185105+amerh12@users.noreply.github.com>
2022-06-29 17:50:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNaloga4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Zaposlen DelavecEna = new Zaposlen();
+
+            DelavecEna.Ime = "Ziga";
+            DelavecEna.Priimek = "Zerjav";
+            DelavecEna.Placa = 1024.35;
+
+            /*DelavecEna.RealnaPlaca(3.5); - NIKJER NI PISAL DA MORAM IZPISAT POVEČANO PLAČO TAKO DA METODO, KAKO TO NAREST IMATE V RAZREDU ZAPOSLEN
+            V TEM KOMENTARJU JE PRIKAZANO KAKO BI KLICAL METODO
+            */
+
+
+            Console.WriteLine("Delavec " + DelavecEna.Ime +  " " + DelavecEna.Priimek + " ima urno postavko: " + DelavecEna.ToString());
+
+        }
+    }
+}
---
+++ Zaposlen.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNaloga4
+{
+    internal class Zaposlen
+    {
+        public string _ime;
+        public string _priimek;
+        public double _placa;
+       
+
+
+        public string Ime
+        {
+            get { return _ime; }
+            set { _ime = value; }
+        }
+        public string Priimek
+        {
+            get { return _priimek; }
+                set { _priimek = value; }
+        }
+        public double Placa
+        {
+            get { return _placa; }
+            set { _placa = value; }
+        }
+
+        public double RealnaPlaca (double podanOdstotek){
+
+
+            double odstotekVdenarju = (podanOdstotek * 100) / _placa;
+            double povecanaPlaca = _placa + odstotekVdenarju;
+            return povecanaPlaca;
+
+        
+        }
+        public override string ToString()
+        {
+
+            double urnaPostavka = _placa / 176;
+
+            return Convert.ToString(urnaPostavka);
+
+
+        }
+
+
+    }
+}
